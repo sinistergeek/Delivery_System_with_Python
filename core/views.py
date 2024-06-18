513df3b2b9bd3dc6edfb496ca5ba6ee799fbcd70
sinistergeek <susanbhattraidon@gmail.com>
2024-06-18 16:31:32 +0545

    Basic_Authentication

--- core/views.py
+++ core/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required 
 
 # Create your views here. 
 
 def home(request):
     return render(request,'home.html')
+
+@login_required()
 def customer_page(request):
     return render(request,'home.html')
+
+@login_required()
 def courier_page(request):
     return render(request,'home.html')
 
